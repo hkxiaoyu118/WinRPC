8c84923e8693c2eab9ee8cb7bd6b6760b4d0b92d
hkxiaoyu118 <hkxiaoyu118@gmail.com>
2019-09-30 17:55:51 +0800

    共享内存模块写完

---
+++ WinRPC/WinRPC/ShareMemory.cpp
@@ -0,0 +1,97 @@
+#include "ShareMemory.h"
+
+
+ShareMemory::ShareMemory(const std::string shareMemName, bool createFile)
+{
+	m_isCreateFile = createFile;
+	m_shareMemName = shareMemName;
+	m_semaphore = CreateSemaphoreA(NULL, 1, 1, NULL);
+	m_fileMapping = INVALID_HANDLE_VALUE;
+	m_shareMemAddress = NULL;
+}
+
+
+ShareMemory::~ShareMemory()
+{
+	UnmapViewOfFile(m_shareMemAddress);
+	CloseHandle(m_semaphore);
+	CloseHandle(m_fileMapping);
+}
+
+/*
+	如果用磁盘文件映射,共享内存不会出现存储器release后出现违规访问的问题,但是会在磁盘上建立一个文件
+	文件的名称由参数shareMemName给定.如果用页文件映射,则不会在磁盘上建立一个文件
+	默认使用内存页文件映射
+*/
+void* ShareMemory::OpenShareMem(void* addr, const unsigned length, DWORD protect)
+{
+	m_shareMemSize = length;
+	HANDLE fileHandle = INVALID_HANDLE_VALUE;
+	//如果使用磁盘文件映射
+	if (m_isCreateFile == true)
+	{
+		fileHandle = CreateFileA(
+			m_shareMemName.c_str(), 
+			GENERIC_READ | GENERIC_WRITE, 
+			0,
+			NULL, 
+			OPEN_ALWAYS, 
+			FILE_ATTRIBUTE_NORMAL, 
+			0
+		);
+	}
+
+	do 
+	{
+		//首先尝试打开对应名称的共享内存
+		//如果打开失败则说明没有该名称的共享内存,然后重新创建一个
+		m_fileMapping = OpenFileMappingA(PAGE_READWRITE, false, m_shareMemName.c_str());
+		if (m_fileMapping != NULL)
+		{
+			break;
+		}
+		m_fileMapping = CreateFileMappingA(fileHandle, NULL, PAGE_READWRITE, 0, length, m_shareMemName.c_str());
+		if (m_fileMapping == NULL)
+		{
+			return NULL;
+		}
+	} while (0);
+	m_shareMemAddress = MapViewOfFileEx(m_fileMapping, protect, 0, 0, length, addr);
+	CloseHandle(fileHandle);
+	return m_shareMemAddress;
+}
+
+int ShareMemory::WriteShareMem(void* dest, void*src, unsigned size)
+{
+	if (!CheckAddress(dest))
+		return -1;
+	int writeCount = (int)m_shareMemAddress + m_shareMemSize - (int)dest;
+	if (writeCount > size)
+		writeCount = size;
+	//利用semaphore进行保护映射的区域（同一进程的不同线程调用时候才进行保护,防止多个线程同时写入）
+	WaitForSingleObject(m_semaphore, INFINITE);
+	memcpy(dest, src, writeCount);
+	ReleaseSemaphore(m_semaphore, 1, NULL);
+	FlushViewOfFile(m_shareMemAddress, writeCount);
+	return writeCount;
+}
+
+int ShareMemory::ReadShareMem(void* src, void*dest, unsigned size)
+{
+	if (!CheckAddress(src))
+		return -1;
+	int readCount = (int)m_shareMemAddress + m_shareMemSize - (int)src;
+	if (readCount > size)
+		readCount = size;
+	memcpy(dest, src, readCount);
+	return readCount;
+}
+
+bool ShareMemory::CheckAddress(void* addr)
+{
+	if (((int)addr < (int)m_shareMemAddress) || ((int)addr > (int)m_shareMemAddress + m_shareMemSize))
+	{
+		return false;
+	}
+	return true;
+}
---
+++ WinRPC/WinRPC/ShareMemory.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <windows.h>
+#include <string>
+#include <iostream>
+
+class ShareMemory
+{
+public:
+	ShareMemory(const std::string shareMemName, bool createFile = false);
+	~ShareMemory();
+
+	void* OpenShareMem(void* addr, const unsigned length, DWORD protect);	//打开共享内存
+	int WriteShareMem(void* dest, void*src, unsigned size);					//写共享内存
+	int ReadShareMem(void* src, void*dest, unsigned size);					//读共享内存
+private:
+	bool CheckAddress(void* addr); //检查内存地址是否合法
+private:
+	std::string m_shareMemName;		//共享内存的名字
+	bool m_isCreateFile;			//文件共享内存or内存页共享内存
+	void* m_shareMemAddress;		//共享内存的基地址
+	HANDLE m_semaphore;				//信号
+	HANDLE m_fileMapping;			//文件句柄
+	unsigned int m_shareMemSize;	//共享内存的大小
+};
